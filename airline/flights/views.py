7a94e9d8ab1785a1ea73a7d1cf9405a93959cd2c
PB Github <phuong.buitan95@gmail.com>
2023-04-02 14:57:41 +0700

    Update all

---
+++ airline/flights/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Flights
+
+# Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flights.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flights.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
