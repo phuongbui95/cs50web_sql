35bd11108db0759da8ba75265407ab9d7334966e
PB Github <phuong.buitan95@gmail.com>
2023-04-02 16:02:44 +0700

    Update admin

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -15,6 +15,14 @@ class Flights(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+    
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flights, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
 
 # class Flights(models.Model):
 #     origin = models.CharField(max_length=64)
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Flights
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Flights, Passenger
 
 # Create your views here.
 def index(request):
@@ -10,5 +13,14 @@ def index(request):
 def flight(request, flight_id):
     flight = Flights.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flights.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
